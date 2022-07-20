542a924f4ea360484d9adc9f7ad96ef08d54a91f
habibagalal <habibagalal392@gmail.com>
2022-07-20 21:40:32 +0200

    init

---
+++ Pyhton/SimpleCalc.py
@@ -0,0 +1,14 @@
+num1 = input("Please Enter Y our First Number !")
+operator = input("Please Enter Your Operator  !")
+num2 = input("Please Enter  Your Second Number !")
+
+if operator == "+":
+    print(num1+num2)
+elif operator == "-":
+    print(num1+num2)
+elif operator == "/":
+    print(num1+num2)
+elif operator == "*":
+    print(num1+num2)
+else:
+    print("Wrong Operator")
